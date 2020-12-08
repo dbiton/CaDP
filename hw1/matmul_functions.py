278c7bcf929057f1eabdc6e71ac19ca78cfa7da8
dbiton <dbiton@gmail.com>
2020-12-08 17:36:07 +0200

    Update matmul_functions.py
    replaced A.shape[0] with mat_size so it's clearer.
    removed parallel tag from njit
    removed os.envio
    added copying of res from host to device and back

--- hw1/matmul_functions.py
+++ hw1/matmul_functions.py
@@ -2,34 +2,35 @@ import numpy as np
 from numba import njit, cuda
 import timeit
 import os
-os.environ['NUMBA_ENABLE_CUDASIM'] = '1'
-
 
 def matmul_transpose_trivial(X):
-    A = np.zeros((X.shape[0], X.shape[0]))
-    for i in range(X.shape[0]):
-        for j in range(X.shape[0]):
+    mat_size = X.shape[0]
+    A = np.zeros((mat_size, mat_size))
+    for i in range(mat_size):
+        for j in range(mat_size):
             for k in range(X.shape[1]):
                 A[i, j] += X[i, k] * X[j, k]
     return A
 
 
-@njit(parallel=True)
+@njit
 def matmul_transpose_numba(X):
-    A = np.zeros((X.shape[0], X.shape[0]))
-    for i in range(X.shape[0]):
-        for j in range(X.shape[0]):
+    A = np.zeros((mat_size, mat_size))
+    for i in range(mat_size):
+        for j in range(mat_size):
             for k in range(X.shape[1]):
                 A[i, j] += X[i, k] * X[j, k]
     return A
 
 
 def matmul_transpose_gpu(X):
-    d_X = cuda.to_device(X)
-
+    mat_size = X.shape[0]
     num_threads = 1024
-    res = np.zeros((X.shape[0], X.shape[0]))
-    matmul_kernel[1, num_threads](d_X, res)
+    res = np.zeros((mat_size, mat_size))
+    d_res = cuda.to_device(res)
+    d_X = cuda.to_device(X)
+    matmul_kernel[1, num_threads](d_X, d_res)
+    res = cuda.to_host(d_res)
     return res
 
 
@@ -48,52 +49,8 @@ def matmul_kernel(A, C):
         for k in range(A.shape[1]):
             cell_value += A[cell_x, k] * A[cell_y, k]
         C[cell_x, cell_y] = cell_value
-"""
-def matmul_transpose_gpu(X):
-    d_X = cuda.to_device(X)
 
-    num_threads = 1024
-    res = np.zeros((X.shape[0], X.shape[0]))
-    matmul_kernel[1, num_threads](d_X, res)
-    return res
-
-
-@cuda.jit
-def matmul_kernel(A, C):
-    tid = cuda.threadIdx.x
-    num_threads = cuda.blockDim.x
-
-    num_cells = mat_size ** 2
-    cells_per_thread = num_cells // num_threads
-    cells_reminder = num_cells % num_threads
-
-    num_cells_handled = cells_per_thread
-
-    # this thread handles the reminder
-    if tid == num_threads - 1:
-        num_cells_handled = cells_reminder
-        print(cells_per_thread, ", ", cells_reminder)
-    # beginning cell
-    cell_begin_idx = cells_per_thread * tid
-
-    # first cell to deal with
-    cell_curr_pos_x = cell_begin_idx // mat_size
-    cell_curr_pos_y = cell_begin_idx % mat_size
-
-    # for each cell hat the thread need do do:
-    for num_cell in range(num_cells_handled):
-        cell_curr_value = 0
-        # get the res[x, y]
-        for k in range(A.shape[1]):
-            cell_curr_value += A[cell_curr_pos_x, k] * A[cell_curr_pos_y, k]
-
-        C[cell_curr_pos_x, cell_curr_pos_y] = cell_curr_value
-        cell_curr_pos_x += 1
-        if cell_curr_pos_x == mat_size:
-            cell_curr_pos_x = 0
-            cell_curr_pos_y += 1
-
-"""
+   
 # this is the comparison function - keep it as it is, don't change X or Y.
 def matmul_comparison():
     X = np.random.randn(784, 128)
